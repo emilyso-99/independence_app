0dcc9e047910b2c4f90a02bf9c90d0ea56660d44
Jeffrey Huang <jeffreyzt.huang@mail.utoronto.ca>
2020-09-05 02:36:18 -0400

    finished text to speech

--- independence_app/lib/main.dart
+++ independence_app/lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:independence_app/speechtotext.dart';
 
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
