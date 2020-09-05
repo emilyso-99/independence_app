16626718f6224d593980a04fdd13b88f132d9b0f
emilyso-99 <emmy4short@gmail.com>
2020-09-05 18:57:09 -0400

    adding functionality

--- independence_app/lib/login.dart
+++ independence_app/lib/login.dart
@@ -1,30 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
 
+const users = const {
+  'dribbble@gmail.com': '12345',
+  'hunter@gmail.com': 'hunter',
+};
+
 class LoginScreen extends StatelessWidget {
+  Duration get loginTime => Duration(milliseconds: 2250);
+
+  Future<String> _authUser(LoginData data) {
+    print('Name: ${data.name}, Password: ${data.password}');
+    // this is where to check in Firebase
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(data.name)) {
+        return 'Username not exists';
+      }
+      if (users[data.name] != data.password) {
+        return 'Password does not match';
+      }
+      return null;
+    });
+  }
+
+  Future<String> _recoverPassword(String name) {
+    print('Name: $name');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(name)) {
+        return 'Username not exists';
+      }
+      return null;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return FlutterLogin(
       title: 'INDEPENDENCE',
-      logo: 'assets/images/ecorp.png',
-      onLogin: (_) => Future(null),
-      onSignup: (_) => Future(null),
+      logo: 'assets/images/ecorp-lightblue.png',
+      onLogin: _authUser,
+      onSignup: _authUser,
       onSubmitAnimationCompleted: () {},
-      onRecoverPassword: (_) => Future(null),
-      messages: LoginMessages(
-        usernameHint: 'Username',
-        passwordHint: 'MedID',
-        confirmPasswordHint: 'Confirm',
-        loginButton: 'LOG IN',
-        signupButton: 'REGISTER',
-        forgotPasswordButton: "What is my medical ID?",
-        recoverPasswordButton: 'HELP ME',
-        goBackButton: 'GO BACK',
-        confirmPasswordError: 'Not match!',
-        recoverPasswordDescription:
-            'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
-        recoverPasswordSuccess: 'Password rescued successfully',
-      ),
+      onRecoverPassword: _recoverPassword,
+      messages: LoginMessages(usernameHint: "NAME", passwordHint: "MedID"),
     );
   }
 }
