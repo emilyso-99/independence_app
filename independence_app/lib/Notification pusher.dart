75fc8e5dac010d1a1397525f1a8d0470733d4e7d
emilyso-99 <emmy4short@gmail.com>
2020-09-05 23:50:55 -0400

    added firebase

--- independence_app/lib/Notification pusher.dart
+++ independence_app/lib/Notification pusher.dart
@@ -1,28 +1,28 @@
-//Daily notifcation at setable time
+// //Daily notifcation at setable time
 
-void _configureCustomReminder(bool value) {
-  if (customNotificationTime != null) {
-    if (value) {
-      var now = new DateTime.now();
-      var notificationTime = new DateTime(now.year, now.month, now.day,
-          customNotificationTime.hour, customNotificationTime.minute);
+// void _configureCustomReminder(bool value) {
+//   if (customNotificationTime != null) {
+//     if (value) {
+//       var now = new DateTime.now();
+//       var notificationTime = new DateTime(now.year, now.month, now.day,
+//           customNotificationTime.hour, customNotificationTime.minute);
 
-      getStore().dispatch(SetReminderAction(
-          time: notificationTime.toIso8601String(),
-          name: custom,
-          repeat: RepeatInterval.Daily));
+//       getStore().dispatch(SetReminderAction(
+//           time: notificationTime.toIso8601String(),
+//           name: custom,
+//           repeat: RepeatInterval.Daily));
 
-      scheduleNotification(
-          flutterLocalNotificationsPlugin, '4', custom, notificationTime);
-    } else {
-      getStore().dispatch(RemoveReminderAction(custom));
-      turnOffNotificationById(flutterLocalNotificationsPlugin, 4);
-    }
-  }
-}
+//       scheduleNotification(
+//           flutterLocalNotificationsPlugin, '4', custom, notificationTime);
+//     } else {
+//       getStore().dispatch(RemoveReminderAction(custom));
+//       turnOffNotificationById(flutterLocalNotificationsPlugin, 4);
+//     }
+//   }
+// }
 
-Future<void> turnOffNotificationById(
-    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin,
-    num id) async {
-  await flutterLocalNotificationsPlugin.cancel(id);
-}
+// Future<void> turnOffNotificationById(
+//     FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin,
+//     num id) async {
+//   await flutterLocalNotificationsPlugin.cancel(id);
+// }
--- independence_app/lib/login.dart
+++ independence_app/lib/login.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
 import 'package:independence_app/Question 2.dart';
+import "globals.dart";
 
 const users = const {
   'dribbble@gmail.com': '12345',
@@ -11,16 +13,41 @@ class LoginScreen extends StatelessWidget {
   Duration get loginTime => Duration(milliseconds: 2250);
 
   Future<String> _authUser(LoginData data) {
-    print('Name: ${data.name}, Password: ${data.password}');
-    // this is where to check in Firebase
-    return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(data.name)) {
-        return 'Username not exists';
-      }
-      if (users[data.name] != data.password) {
-        return 'Password does not match';
-      }
-      return null;
+    // print('Name: ${data.name}, Password: ${data.password}');
+    // // this is where to check in Firebase
+    // return Future.delayed(loginTime).then((_) {
+    //   if (!users.containsKey(data.name)) {
+    //     return 'Username not exists';
+    //   }
+    //   if (users[data.name] != data.password) {
+    //     return 'Password does not match';
+    //   }
+    //   return null;
+    // });
+    Firestore.instance
+        .collection("patient_info")
+        .getDocuments()
+        .then((querySnapshot) {
+      querySnapshot.documents.forEach((result) {
+        if (result.data["name"] == data.name) {
+          GlobalData.medid = result.data["MedID"];
+          GlobalData.username = result.data["name"];
+          Navigator.push(GlobalData.context,
+              MaterialPageRoute(builder: (context) => QuestionTwo()));
+          print(result.data);
+        } else {
+          Firestore.instance.collection("patient_data").add({
+            "name": data.name,
+            "MedID": data.password,
+          });
+          GlobalData.username = data.name;
+          GlobalData.medid = data.password;
+          Navigator.push(GlobalData.context,
+              MaterialPageRoute(builder: (context) => QuestionTwo()));
+          print(result.data);
+        }
+        return null;
+      });
     });
   }
 
@@ -36,18 +63,21 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    GlobalData.context = context;
     return FlutterLogin(
       title: 'INDEPENDENCE',
       logo: 'assets/images/ecorp-lightblue.png',
-      // onLogin: _authUser
-      onLogin: (_) => Navigator.push(
-          context, MaterialPageRoute(builder: (context) => QuestionTwo())),
-      // onSignup: _authUser,
-      onSignup: (_) => Navigator.push(
-          context, MaterialPageRoute(builder: (context) => QuestionTwo())),
+      onLogin: _authUser
+      // onLogin: (_) => Navigator.push(
+      //     context, MaterialPageRoute(builder: (context) => QuestionTwo())),
+      ,
+      onSignup: null,
+      // onSignup: (_) => Navigator.push(
+      //     context, MaterialPageRoute(builder: (context) => QuestionTwo())),
       onSubmitAnimationCompleted: () {},
       onRecoverPassword: _recoverPassword,
       messages: LoginMessages(usernameHint: "NAME", passwordHint: "MedID"),
+      emailValidator: null,
     );
   }
 }
