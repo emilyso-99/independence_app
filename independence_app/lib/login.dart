cd80f0349b37786037633bf8043f7b4734c333c9
emilyso-99 <emmy4short@gmail.com>
2020-09-06 01:09:15 -0400

    adding routing

--- independence_app/lib/login.dart
+++ independence_app/lib/login.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
-import 'package:independence_app/Question 2.dart';
+import 'questions/Question 2.dart';
 import "globals.dart";
 
 const users = const {
--- independence_app/lib/speechtotext.dart
+++ independence_app/lib/speechtotext.dart
@@ -1,7 +1,13 @@
+import 'package:independence_app/globals.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'package:flutter/material.dart';
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:highlight_text/highlight_text.dart';
+import 'questions/Question 3.dart';
+import 'questions/Question 4.dart';
+import 'questions/Question 5.dart';
+import 'questions/Question 6.dart';
+import 'questions/Question 7 - sign off.dart';
 
 class SpeechToText extends StatefulWidget {
   @override
@@ -61,41 +67,39 @@ class _SpeechToTextState extends State<SpeechToText> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.height / 2,
-      width: MediaQuery.of(context).size.width,
-      child: new Scaffold(
-        
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: AvatarGlow(
-        animate: _isListening,
-        glowColor: Theme.of(context).primaryColor,
-        endRadius: 75.0,
-        duration: const Duration(milliseconds: 2000),
-        repeatPauseDuration: const Duration(milliseconds: 100),
-        repeat: true,
-        child: FloatingActionButton(
-          onPressed: _listen,
-          child: Icon(_isListening ? Icons.mic : Icons.mic_none),
-        ),
-      ),
-      body: SingleChildScrollView(
-        reverse: true,
-        
-        child: Container(
-          child: TextHighlight(
-            text: _text,
-            textAlign: TextAlign.center,
-            words: _highlights,
-            textStyle: const TextStyle(
-              fontSize: 12.0,
-              color: Colors.black,
-              fontWeight: FontWeight.w400,
+        height: MediaQuery.of(context).size.height / 2,
+        width: MediaQuery.of(context).size.width,
+        child: new Scaffold(
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerFloat,
+          floatingActionButton: AvatarGlow(
+            animate: _isListening,
+            glowColor: Theme.of(context).primaryColor,
+            endRadius: 75.0,
+            duration: const Duration(milliseconds: 2000),
+            repeatPauseDuration: const Duration(milliseconds: 100),
+            repeat: true,
+            child: FloatingActionButton(
+              onPressed: _listen,
+              child: Icon(_isListening ? Icons.mic : Icons.mic_none),
             ),
           ),
-        ),
-      ),
-    )
-    );
+          body: SingleChildScrollView(
+            reverse: true,
+            child: Container(
+              child: TextHighlight(
+                text: _text,
+                textAlign: TextAlign.center,
+                words: _highlights,
+                textStyle: const TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 
   void _listen() async {
@@ -108,7 +112,7 @@ class _SpeechToTextState extends State<SpeechToText> {
         setState(() => _isListening = true);
         _speech.listen(
           onResult: (val) => setState(() {
-            if (val.recognizedWords.isNotEmpty){
+            if (val.recognizedWords.isNotEmpty) {
               _text = val.recognizedWords;
             }
           }),
@@ -117,6 +121,26 @@ class _SpeechToTextState extends State<SpeechToText> {
     } else {
       setState(() => _isListening = false);
       _speech.stop();
+      if (GlobalData.question == 2) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => QuestionThree()));
+      }
+      if (GlobalData.question == 3) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => QuestionFour()));
+      }
+      if (GlobalData.question == 4) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => QuestionFive()));
+      }
+      if (GlobalData.question == 5) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => QuestionSix()));
+      }
+      if (GlobalData.question == 6) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => QuestionSeven()));
+      }
     }
   }
 }
